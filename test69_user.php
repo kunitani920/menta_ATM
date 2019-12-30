88fe7118fbbceee312296132214beaec4d53a61a
tani <kuni.tani.920@gmail.com>
2019-12-30 19:05:29 +0900

    5th commit

--- test69_user.php
+++ test69_user.php
@@ -1,8 +1,11 @@
 <?php
 //ver2.本体（ver3）に合わせて、残高追加
 //ver3.本体（ver4）に合わせて、クラス内に全て移動。ユーザー情報は初期値でセット。残高をmoney->balanceに変更
+//ver4.本体（ver5）に合わせて、checkUserList,getById作成、getUser削除
 class User {
-    public $user_list = array(
+    //ユーザー数
+    const USER_MAX = 2;
+    public static $user_list = array(
         1 => array(
             "id" => "1",
             "password" => "pass",
@@ -17,8 +20,17 @@ class User {
         )
     );
 
-    public function getUser() {
-        return $this->user_list;
+    public function checkUserList($id) {
+        for($i = 1; $i <= self::USER_MAX; $i++) {
+            if(self::$user_list[$i]['id'] === $id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getUserById($id) {
+        return self::$user_list[$id];
     }
 }
 
