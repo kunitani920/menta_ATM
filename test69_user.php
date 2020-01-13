52961e680f8878b5cea60f2784082349d30a577a
tani <kuni.tani.920@gmail.com>
2020-01-13 13:55:39 +0900

    9.1th commit/test69_user 一部変更

--- test69_user.php
+++ test69_user.php
@@ -3,8 +3,9 @@
 //ver3.本体（ver4）に合わせて、クラス内に全て移動。ユーザー情報は初期値でセット。残高をmoney->balanceに変更
 //ver4.本体（ver5）に合わせて、checkUserList,getById作成、getUser削除
 //ver5.本体（ver7）の定数を利用出来るとわかり、USER_MAXを利用
+//ver6.本体（ver9）に合わせ、カプセル化のため、プロパティをprivateへ
 class User {
-    public static $user_list = array(
+    private static $user_list = array(
         1 => array(
             "id" => "1",
             "password" => "pass",
